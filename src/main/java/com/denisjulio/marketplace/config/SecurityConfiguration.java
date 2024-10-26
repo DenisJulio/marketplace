d417e96b3df78efbc1b2c36ebfb81a3e1848a0b2
Denis Julio <djms1234@gmail.com>
2024-10-26 12:42:29 -0300

    Adiciona configurações para login e realiza mudanças na estilisação da pagina detalhes-anuncio

--- src/main/java/com/denisjulio/marketplace/config/SecurityConfiguration.java
+++ src/main/java/com/denisjulio/marketplace/config/SecurityConfiguration.java
@@ -1,37 +1,54 @@
 package com.denisjulio.marketplace.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private static String[] PERMITTED_PATHS = { "/", "/anuncios/**" };
+    private static String[] STATIC_RESOURCES = { "/icons/**", "/images/**", "/webjars/**", "/style.css" };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/icons/**", "/images/**", "/webjars/**", "/style.css").permitAll()
+                .requestMatchers(PERMITTED_PATHS).permitAll()
+                .requestMatchers(STATIC_RESOURCES).permitAll()
                 .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/login").permitAll());
         return http.build();
     }
 
     @Bean
-    UserDetailsService userDetailsService() {
-        var userdetails = User.withDefaultPasswordEncoder()
-                .username("Denis")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(userdetails);
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        var userQuery = """
+                SELECT email, password, enabled
+                FROM usuarios
+                WHERE email = ?
+                 """;
+        var authQuery = """
+                SELECT email, autorizacao
+                FROM autorizacoes
+                WHERE email = ?
+                 """;
+        var users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery(userQuery);
+        users.setAuthoritiesByUsernameQuery(authQuery);
+        return users;
     }
 }
